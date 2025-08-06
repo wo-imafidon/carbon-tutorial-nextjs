2365bdcf66f5bc116937c1aeae2cd6725d5e6411
Winnie Imafidon <woimafidon@li-a40ab3cc-23bd-11b2-a85c-9e53b1066ade.ibm.com>
2025-08-06 16:29:48 +0100

    feat(tutorial): complete step 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+import './globals.scss';
+import { Providers } from './providers';
 
 export const metadata = {
   title: 'Carbon + Next13',
-  description: 'IBM Carbon Tutorial with NextJS 13',
-}
+  description: 'IBM Carbon Tutorial with Next.js 13',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* insert providers segment into the RoootLayer */}
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+'use client';
+// import { Button } from '@carbon/react';
+import LandingPage from './home/page';
+import RepoPage from './repos/page';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+// export default function Home() {
+//   return (
+// //     <div>
+// // Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+// //     </div>
 
-  )
+// <Button>Button</Button>
+
+//   )
+// }
+
+export default function Page() {
+  return <LandingPage />;
+  return <RepoPage />;
 }
